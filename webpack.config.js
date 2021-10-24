e6c7ba29dec9fef7ce047eab13a7be44e6ac8bb5
Mitya <mitro_fan@inbox.ru>
2021-10-25 01:19:02 +0300

    preparing

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+const Dotenv = require("dotenv-webpack")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const fse = require("fs-extra")
 
-module.exports = {
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy files", function () {
+      fse.copySync("./app/main.css", "./dist/main.css")
+      fse.copySync("./app/images", "./dist/images")
+    })
+  }
+}
+
+config = {
   entry: "./app/Main.js",
   output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "dist")
+    publicPath: "/",
+    path: path.resolve(__dirname, "app"),
+    filename: "bundled.js"
   },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "app/start-index.html",
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ],
   mode: "development",
-  devServer: {
-    port: 3000,
-    static: "app",
-    hot: true,
-    historyApiFallback: { index: "index.html" }
-  },
   module: {
     rules: [
       {
@@ -29,20 +48,26 @@ module.exports = {
   }
 }
 
-// const path = require("path")
+if (currentTask == "webpackDev" || currentTask == "dev") {
+  config.devtool = "source-map"
+  config.devServer = {
+    port: 3030,
+    // contentBase: path.join(__dirname, "app"),
+    static: "app",
+    hot: true,
+    historyApiFallback: { index: "index.html" }
+  }
+}
 
-// module.exports = {
-//   entry: "./app/Main.js",
-//   output: {
-//     publicPath: "/",
-//     path: path.resolve(__dirname, "app"),
-//     filename: "bundled.js"
-//   },
-//   mode: "development",
-//   devtool: "source-map",
-//   devServer: {
-//     port: 3000,
-//     contentBase: path.join(__dirname, "app"),
-//     hot: true,
-//     historyApiFallback: { index: "index.html" }
-//   }
+if (currentTask == "webpackBuild") {
+  config.plugins.push(new CleanWebpackPlugin(), new RunAfterCompile())
+  config.mode = "production"
+  config.output = {
+    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js"
+  }
+}
+
+module.exports = config
